2cb5da20bb8986e72e2e1b286334c508a32ac1e9
Shanon McQuay <hi@shan.dog>
2025-07-08 14:07:40 +1000

    feat(programs): add lazygit

---
+++ modules/programs/cli/lazygit.nix
@@ -0,0 +1,25 @@
+{delib, ...}:
+delib.module {
+  # version control ui
+  name = "programs.cli.lazygit";
+
+  options = {myconfig, ...} @ args: delib.singleEnableOption myconfig.host.isDesktop args;
+
+  home.ifEnabled = {
+    programs.lazygit = {
+      enable = true;
+      settings = {
+        git = {
+          # annoying as it prompts for authentication
+          autoFetch = false;
+        };
+      };
+    };
+
+    home = {
+      shellAliases = {
+        lg = "lazygit";
+      };
+    };
+  };
+}
