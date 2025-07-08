2536ed63d747a6bd7f170e6272c63eac39c23a02
Shanon McQuay <hi@shan.dog>
2025-07-08 13:58:42 +1000

    feat(programs): add git aliases

--- modules/programs/cli/git.nix
+++ modules/programs/cli/git.nix
@@ -23,5 +23,26 @@ delib.module {
       userName = myconfig.constants.username;
       userEmail = myconfig.constants.email;
     };
+
+    home = {
+      shellAliases = {
+        ga = "git add";
+        gars = "git add . && git reset --hard";
+        gc = "git commit";
+        gca = "git commit --amend";
+        gcl = "git clone";
+        gcm = "git commit -m";
+        gco = "git checkout";
+        gcp = "git cherry-pick";
+        gd = "git diff";
+        gdc = "git diff --cached";
+        glg = "git log --stat";
+        glr = "git pull --rebase";
+        gpo = "git push origin \"$(git symbolic-ref --short HEAD)\"";
+        grh = "git reset HEAD";
+        grm = "git rm";
+        gs = "git status";
+      };
+    };
   };
 }
