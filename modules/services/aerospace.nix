5bee5750cd46fcbdb2d8d5d22019e946d1c449c1
Shanon McQuay <hi@shan.dog>
2025-07-09 14:07:30 +1000

    feat(services): add aerospace

---
+++ modules/services/aerospace.nix
@@ -0,0 +1,58 @@
+{delib, ...}:
+delib.module {
+  # window manager
+  name = "services.aerospace";
+
+  options = delib.singleEnableOption true;
+
+  darwin.ifEnabled = {
+    services.aerospace = {
+      enable = true;
+
+      settings = {
+        accordion-padding = 30;
+        default-root-container-layout = "tiles";
+        default-root-container-orientation = "auto";
+
+        gaps = {
+          inner.horizontal = 8;
+          inner.vertical = 8;
+          outer.left = 8;
+          outer.bottom = 8;
+          outer.top = 8;
+          outer.right = 8;
+        };
+
+        mode.main.binding = {
+          alt-shift-ctrl-cmd-1 = "workspace 1";
+          alt-shift-ctrl-cmd-2 = "workspace 2";
+          alt-shift-ctrl-cmd-3 = "workspace 3";
+          alt-shift-ctrl-cmd-4 = "workspace 4";
+          alt-shift-ctrl-cmd-5 = "workspace 5";
+          alt-shift-ctrl-cmd-6 = "workspace 6";
+          alt-shift-ctrl-cmd-7 = "workspace 7";
+          alt-shift-ctrl-cmd-8 = "workspace 8";
+          alt-shift-ctrl-cmd-9 = "workspace 9";
+
+          alt-shift-ctrl-1 = "move-node-to-workspace 1";
+          alt-shift-ctrl-2 = "move-node-to-workspace 2";
+          alt-shift-ctrl-3 = "move-node-to-workspace 3";
+          alt-shift-ctrl-4 = "move-node-to-workspace 4";
+          alt-shift-ctrl-5 = "move-node-to-workspace 5";
+          alt-shift-ctrl-6 = "move-node-to-workspace 6";
+          alt-shift-ctrl-7 = "move-node-to-workspace 7";
+          alt-shift-ctrl-8 = "move-node-to-workspace 8";
+          alt-shift-ctrl-9 = "move-node-to-workspace 9";
+
+          alt-shift-ctrl-a = "layout accordion horizontal vertical";
+          alt-shift-ctrl-t = "layout tiles horizontal vertical";
+
+          alt-shift-ctrl-m = "focus left";
+          alt-shift-ctrl-n = "focus down";
+          alt-shift-ctrl-e = "focus up";
+          alt-shift-ctrl-i = "focus right";
+        };
+      };
+    };
+  };
+}
