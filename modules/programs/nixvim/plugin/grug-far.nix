59c0518ba1bf49f8e997c528d95e20f77d0c67da
Shanon McQuay <hi@shan.dog>
2025-07-10 14:19:35 +1000

    feat(nixvim): add grug-far

---
+++ modules/programs/nixvim/plugin/grug-far.nix
@@ -0,0 +1,21 @@
+{delib, ...}:
+delib.module {
+  # search and replace
+  name = "programs.nixvim.plugins.grug-far";
+  options = delib.singleEnableOption true;
+
+  home.ifEnabled.programs.nixvim = {
+    plugins.grug-far = {
+      enable = true;
+
+      # delay loading until requested
+      lazyLoad.settings.keys = [
+        {
+          __unkeyed-1 = "<leader>cs";
+          __unkeyed-2 = "<cmd>lua require(\"grug-far\").open()<cr>";
+          desc = "search and replace";
+        }
+      ];
+    };
+  };
+}
