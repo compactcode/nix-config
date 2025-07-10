89a0d245aa363cceee2dc815580b83629546dd32
Shanon McQuay <hi@shan.dog>
2025-07-11 09:06:33 +1000

    feat(rice): enable darwin styling

--- rices/catppuccin-mocha/default.nix
+++ rices/catppuccin-mocha/default.nix
@@ -7,7 +7,50 @@
 delib.rice {
   name = "catppuccin-mocha";
 
-  darwin.imports = [inputs.stylix.darwinModules.stylix];
+  darwin = {
+    imports = [inputs.stylix.darwinModules.stylix];
+
+    stylix = {
+      enable = true;
+
+      autoEnable = false;
+
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+
+      fonts = {
+        emoji = {
+          package = pkgs.noto-fonts-emoji;
+          name = "Noto Color Emoji";
+        };
+
+        monospace = {
+          package = pkgs.nerd-fonts.sauce-code-pro;
+          name = "Sauce Code Pro Nerd Font";
+        };
+
+        sansSerif = {
+          package = pkgs.rubik;
+          name = "Rubik";
+        };
+
+        serif = {
+          package = pkgs.noto-fonts;
+          name = "Noto Serif";
+        };
+      };
+
+      # dark mode
+      polarity = "dark";
+    };
+  };
+
+  home = {
+    stylix.targets = {
+      bat.enable = true;
+      btop.enable = true;
+      kitty.enable = true;
+    };
+  };
 
   nixos = {
     imports = [inputs.stylix.nixosModules.stylix];
@@ -43,6 +86,10 @@ delib.rice {
 
       # dark mode
       polarity = "dark";
+
+      targets = {
+        console = true;
+      };
     };
   };
 }
