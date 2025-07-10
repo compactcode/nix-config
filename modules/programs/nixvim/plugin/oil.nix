4a665f64c8df22e478bb7a77c52309d8d4e60064
Shanon McQuay <hi@shan.dog>
2025-07-10 15:11:00 +1000

    feat(nixvim): add oil

---
+++ modules/programs/nixvim/plugin/oil.nix
@@ -0,0 +1,35 @@
+{delib, ...}:
+delib.module {
+  # file explorer
+  name = "programs.nixvim.plugins.oil";
+
+  options = delib.singleEnableOption true;
+
+  home.ifEnabled.programs.nixvim = {
+    keymaps = [
+      {
+        key = "<leader>e";
+        action = "<cmd>Oil<cr>";
+        options = {desc = "explore files";};
+      }
+    ];
+
+    plugins.oil = {
+      enable = true;
+
+      settings = {
+        # dont prompt when making non destructive changes
+        skip_confirm_for_simple_edits = true;
+        view_options = {
+          # show files and directories starting with "."
+          show_hidden = true;
+        };
+      };
+
+      # delay loading until requested
+      lazyLoad.settings = {
+        cmd = "Oil";
+      };
+    };
+  };
+}
