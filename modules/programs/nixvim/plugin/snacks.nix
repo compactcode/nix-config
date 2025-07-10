d9e31df68614fc403acd8e178df1dfaf649c1f4c
Shanon McQuay <hi@shan.dog>
2025-07-10 17:32:41 +1000

    feat(nixvim): add snacks

---
+++ modules/programs/nixvim/plugin/snacks.nix
@@ -0,0 +1,45 @@
+{delib, ...}:
+delib.module {
+  # collection of plugins
+  name = "programs.nixvim.plugins.snacks";
+
+  options = delib.singleEnableOption true;
+
+  home.ifEnabled.programs.nixvim = {
+    keymaps = [
+      # {
+      #   key = "<leader>t";
+      #   action = "<cmd>lua Snacks.terminal.toggle()<cr>";
+      #   mode = ["n"];
+      #   options = {desc = "open terminal";};
+      # }
+      {
+        key = "<C-/>";
+        action = "<cmd>close<cr>";
+        mode = ["t"];
+        options = {desc = "hide terminal";};
+      }
+    ];
+
+    plugins.snacks = {
+      enable = true;
+
+      settings = {
+        # upgrade input prompt
+        input = {
+          enable = true;
+        };
+
+        # terminal utilities
+        terminal = {
+          enable = true;
+        };
+      };
+
+      # delay loading until the ui is loaded
+      lazyLoad.settings = {
+        event = "DeferredUIEnter";
+      };
+    };
+  };
+}
