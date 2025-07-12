9ef5877a8d5117acb39dbe7ef10e8f9bae7ed877
Shanon McQuay <hi@shan.dog>
2025-07-12 19:27:18 +1000

    fix(services): incorrect option config

--- modules/programs/swaylock.nix
+++ modules/programs/swaylock.nix
@@ -3,12 +3,18 @@ delib.module {
   # screen locker
   name = "progams.swaylock";
 
-  options = {myconfig, ...}:
-    with delib; {
+  options = {myconfig, ...}: {
+    programs.swaylock = with delib; {
       enable = boolOption myconfig.services.hyprland.enable;
     };
+  };
 
   home.ifEnabled = {
     programs.swaylock.enable = true;
+  };
+
+  nixos.ifEnabled = {
+    # allow swaylock to perform authentication
+    security.pam.services.swaylock = {};
   };
 }
