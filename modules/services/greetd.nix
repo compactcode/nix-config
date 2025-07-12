5e095cc8b1ffee26ce8f2284a7a8ef9a58795623
Shanon McQuay <hi@shan.dog>
2025-07-13 06:52:39 +1000

    feat(services): add greetd

---
+++ modules/services/greetd.nix
@@ -0,0 +1,30 @@
+{
+  delib,
+  config,
+  lib,
+  ...
+}:
+delib.module {
+  # login manager
+  name = "services.greetd";
+
+  options = {myconfig, ...}: {
+    services.greetd = with delib; {
+      enable = boolOption myconfig.services.hyprland.enable;
+    };
+  };
+
+  nixos.ifEnabled = {myconfig, ...}: {
+    services.greetd = {
+      enable = true;
+      settings = rec {
+        initial_session = {
+          command = "${lib.getExe config.programs.uwsm.package} start hyprland-uwsm.desktop";
+          user = myconfig.users.primary.id;
+        };
+        # skip first login since boot requires luks password
+        default_session = initial_session;
+      };
+    };
+  };
+}
