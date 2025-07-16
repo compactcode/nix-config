53d0070948a037373b0b4606ee5400851a732751
Shanon McQuay <hi@shan.dog>
2025-07-16 11:20:36 +1000

    feat(programs): nixvim add base configuration

--- modules/programs/nixvim/default.nix
+++ modules/programs/nixvim/default.nix
@@ -24,6 +24,82 @@ delib.module {
         };
       };
 
+      globals = {
+        mapleader = " ";
+        maplocalleader = "\\";
+      };
+
+      # vanilla keymaps
+      keymaps = [
+        {
+          key = "<c-left>";
+          action = "<C-w>h";
+        }
+        {
+          key = "<c-down>";
+          action = "<C-w>j";
+        }
+        {
+          key = "<c-up>";
+          action = "<C-w>k";
+        }
+        {
+          key = "<c-right>";
+          action = "<C-w>l";
+        }
+        {
+          key = "<leader>uc";
+          action = "<cmd>nohlsearch<cr>";
+          options = {desc = "clear search highlight";};
+        }
+        {
+          key = "<leader>fc";
+          action = "<cmd>let @+ = expand('%')<cr>";
+          options = {desc = "copy the current path to clipboard";};
+        }
+        {
+          key = "<leader>w";
+          action = "<cmd>w<cr>";
+          options = {desc = "save file";};
+        }
+        {
+          key = "<leader>W";
+          action = "<cmd>wa<cr>";
+          options = {desc = "save all files";};
+        }
+        {
+          key = "<leader>y";
+          action = "\"+y";
+          options = {desc = "copy to system clipboard";};
+        }
+        {
+          key = "<leader>q";
+          action = "<cmd>qa!<cr>";
+          options = {desc = "exit immediately";};
+        }
+      ];
+
+      # base options
+      opts = {
+        backup = false; # disable file versioning
+        expandtab = true; # convert tabs to spaces
+        ignorecase = true; # ignore case by default
+        foldenable = false; # disable code folding
+        list = true; # enable listchars
+        listchars = {
+          nbsp = "+"; # display non breaking spaces
+          tab = ">~"; # display tabs
+          trail = "·"; # display any trailing spaces
+        };
+        number = true; # show line numbers
+        shiftwidth = 2; # use 2 spaces for tab
+        smartcase = true; # don't ignore case if a capital is typed
+        softtabstop = 2; # use 2 spaces for tab
+        swapfile = false; # disable file versioning
+        tabstop = 2; # use 2 spaces for tab
+        writebackup = false; # disable file versioning
+      };
+
       plugins = {
         # lazy loading
         lz-n.enable = true;
