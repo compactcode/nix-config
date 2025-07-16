f261b2172bd5bcfb33390f9dbbc7ee3f881b5515
Shanon McQuay <hi@shan.dog>
2025-07-17 06:25:06 +1000

    feat(base): add xdg preferred applications

--- modules/base/xdg.nix
+++ modules/base/xdg.nix
@@ -18,6 +18,30 @@ delib.module {
     xdg = {
       enable = true;
 
+      # set preferred applications
+      mimeApps = {
+        enable = true;
+        defaultApplications = {
+          "application/pdf" = "org.pwmt.zathura.desktop";
+          "image/gif" = "imv.desktop";
+          "image/jpeg" = "imv.desktop";
+          "image/png" = "imv.desktop";
+          "image/svg+xml" = "imv.desktop";
+          "image/tiff" = "imv.desktop";
+          "image/webp" = "imv.desktop";
+          "inode/directory" = "yazi.desktop";
+          "text/html" = "firefox.desktop";
+          "text/markdown" = "nvim.desktop";
+          "text/plain" = "nvim.desktop";
+          "video/mp4" = "mpv.desktop";
+          "video/quicktime" = "mpv.desktop";
+          "video/x-matroska" = "mpv.desktop";
+          "video/x-ms-wmv" = "mpv.desktop";
+          "x-scheme-handler/http" = "firefox.desktop";
+          "x-scheme-handler/https" = "firefox.desktop";
+        };
+      };
+
       # create default desktop directories
       userDirs = {
         enable = true;
