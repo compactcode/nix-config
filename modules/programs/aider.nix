3ed6a2804b6026e0a623d36900614e5be407f9b6
Shanon McQuay <hi@shan.dog>
2025-07-17 09:56:30 +1000

    refactor(rice): move aider styles to module
    couldn't get access to the stylix lib in the rice

--- modules/programs/aider.nix
+++ modules/programs/aider.nix
@@ -1,4 +1,5 @@
 {
+  config,
   delib,
   pkgs,
   ...
@@ -12,14 +13,24 @@ delib.module {
     extraConfig = attrsOption {};
   };
 
-  home.ifEnabled = {cfg, ...}: {
+  home.ifEnabled = {
     home = {
-      file.".aider.conf.yml".source =
+      file.".aider.conf.yml".source = with config.lib.stylix.colors.withHashtag;
         (pkgs.formats.yaml {}).generate "aider.conf.yml" {
           # disable automatic commit generation
           auto-commits = false;
-        }
-        // cfg.extraConfig;
+
+          # styling with sylix
+          user-input-color = base0B;
+          tool-output-color = base05;
+          tool-error-color = base08;
+          tool-warning-color = base0A;
+          assistant-output-color = base0D;
+          completion-menu-color = base05;
+          completion-menu-bg-color = base00;
+          completion-menu-current-color = base00;
+          completion-menu-current-bg-color = base0D;
+        };
 
       packages = [
         pkgs.aider-chat-with-playwright
