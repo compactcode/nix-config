2638d8e5a09c1a50a52cbf37fc589286256228af
Shanon McQuay <hi@shan.dog>
2025-07-21 07:08:26 +1000

    chore(programs): disable obsidian config
    https://github.com/nix-community/home-manager/issues/7406
    
    need a way to configure app opening and sync first

--- modules/programs/obsidian.nix
+++ modules/programs/obsidian.nix
@@ -1,4 +1,8 @@
-{delib, ...}:
+{
+  delib,
+  pkgs,
+  ...
+}:
 delib.module {
   # markdown viewer
   name = "programs.obsidian";
@@ -6,8 +10,33 @@ delib.module {
   options = delib.singleEnableOption true;
 
   home.ifEnabled = {
-    programs.obsidian = {
-      enable = true;
-    };
+    home.packages = [pkgs.obsidian];
+
+    # TODO: Resolve https://github.com/nix-community/home-manager/issues/7406
+    # programs.obsidian = {
+    #   enable = true;
+    #   defaultSettings = {
+    #     corePlugins = [
+    #       "command-palette"
+    #       "editor-status"
+    #       "file-explorer"
+    #       "file-recovery"
+    #       "global-search"
+    #       "outline"
+    #       "page-preview"
+    #       "switcher"
+    #       "tag-pane"
+    #       "sync"
+    #     ];
+    #   };
+    #   vaults = {
+    #     personal = {
+    #       target = "${homeconfig.xdg.userDirs.documents}/personal";
+    #     };
+    #     zepto = {
+    #       target = "${homeconfig.xdg.userDirs.documents}/zepto";
+    #     };
+    #   };
+    # };
   };
 }
