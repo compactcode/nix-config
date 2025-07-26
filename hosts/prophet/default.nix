ea5d8d3ab183bd902bdeaa2cfae160d92306cdb4
Shanon McQuay <hi@shan.dog>
2025-07-26 15:06:30 +1000

    refactor: replace custom hardware with nixos-hardware for prophet

--- hosts/prophet/default.nix
+++ hosts/prophet/default.nix
@@ -1,7 +1,11 @@
 # CPU: Intel Core i7-8565U
 # GPU: Nvidia Quadro P520
 # Motherboard: ThinkPad P43s
-{delib, ...}:
+{
+  delib,
+  inputs,
+  ...
+}:
 delib.host {
   name = "prophet";
 
@@ -37,48 +41,25 @@ delib.host {
   home.home.stateVersion = "24.05";
 
   nixos = {
-    boot = {
-      # disable nvidia gpu
-      blacklistedKernelModules = ["nouveau" "nvidia" "nvidia_drm" "nvidia_modeset"];
+    imports = [
+      # configure intel cpu
+      inputs.nixos-hardware.nixosModules.common-cpu-intel
+      # configure intel gpu
+      inputs.nixos-hardware.nixosModules.common-gpu-intel
+      # configure ssd
+      inputs.nixos-hardware.nixosModules.common-pc-ssd
+      # disable nvidia gpu (to save power)
+      inputs.nixos-hardware.nixosModules.common-gpu-nvidia-disable
+    ];
 
-      # disable nvidia gpu
-      extraModprobeConfig = ''
-        blacklist nouveau
-        options nouveau modeset=0
-      '';
-
-      initrd = {
-        availableKernelModules = ["ahci" "nvme" "sd_mod" "usb_storage" "usbhid" "xhci_pci"];
-      };
+    # enable gpu acceleration
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
     };
 
-    hardware = {
-      # enable microcode updates
-      cpu.intel.updateMicrocode = true;
-      # enable gpu acceleration
-      graphics = {
-        enable = true;
-        enable32Bit = true;
-      };
-    };
-
-    services = {
-      # fingerprint scanner
-      fprintd.enable = true;
-      # periodic ssd maintenance
-      fstrim.enable = true;
-      # disable nvidia gpu
-      udev.extraRules = ''
-        # Remove NVIDIA USB xHCI Host Controller devices, if present
-        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
-        # Remove NVIDIA USB Type-C UCSI devices, if present
-        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
-        # Remove NVIDIA Audio devices, if present
-        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-        # Remove NVIDIA VGA/3D controller devices
-        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
-      '';
-    };
+    # fingerprint scanner
+    services.fprintd.enable = true;
 
     networking.hostName = "prophet";
 
