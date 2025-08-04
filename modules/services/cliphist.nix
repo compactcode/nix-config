af0fee2ffa00f84291fd7843c0cbdedc02868547
Shanon McQuay <hi@shan.dog>
2025-08-05 07:24:51 +1000

    feat: add cli clipboard utilities

--- modules/services/cliphist.nix
+++ modules/services/cliphist.nix
@@ -1,4 +1,8 @@
-{delib, ...}:
+{
+  delib,
+  pkgs,
+  ...
+}:
 delib.module {
   # clipboard manager
   name = "services.cliphist";
@@ -6,6 +10,10 @@ delib.module {
   options = delib.singleEnableOption false;
 
   home.ifEnabled = {
+    home.packages = [
+      pkgs.wl-clipboard # clipboard utilities
+    ];
+
     services.cliphist.enable = true;
   };
 }
