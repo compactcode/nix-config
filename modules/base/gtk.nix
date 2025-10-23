8c2a8c8ea508e3af674d56912d44ebec886daccb
Shanon McQuay <hi@shan.dog>
2025-10-24 09:30:35 +1100

    fix: missing gtk icons
    noticed in pinta

--- modules/base/gtk.nix
+++ modules/base/gtk.nix
@@ -1,11 +1,22 @@
-{delib, ...}:
+{
+  delib,
+  pkgs,
+  ...
+}:
 delib.module {
   name = "gtk";
 
   options = delib.singleEnableOption false;
 
   home.ifEnabled = {
-    gtk.enable = true;
+    gtk = {
+      enable = true;
+
+      iconTheme = {
+        name = "Adwaita";
+        package = pkgs.adwaita-icon-theme;
+      };
+    };
 
     stylix.targets.gtk = {
       enable = true;
