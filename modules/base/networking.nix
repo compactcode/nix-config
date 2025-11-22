5a6973d917760ac62489b6c963fb2f2815c7bfe2
Shanon McQuay <hi@shan.dog>
2025-11-22 21:31:51 +1100

    feat: add dns utils for network debugging

--- modules/base/networking.nix
+++ modules/base/networking.nix
@@ -1,10 +1,18 @@
-{delib, ...}:
+{
+  delib,
+  pkgs,
+  ...
+}:
 delib.module {
   name = "networking";
 
   options = delib.singleEnableOption true;
 
   nixos.ifEnabled = {
+    environment.systemPackages = [
+      pkgs.dnsutils
+    ];
+
     networking = {
       # enable firewall
       firewall.enable = true;
